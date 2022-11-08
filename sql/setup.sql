552162ec2716f47d95f4fa93f1413df9ab4ffa54
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-07 17:04:32 -0800

    implement data setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,20 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS author CASCADE;
+
+CREATE TABLE author (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    dob DATE,
+    pob VARCHAR
+);
+
+INSERT INTO 
+    author (name, dob, pob)
+VALUES
+-- ('Namy Name', 'YYYY-MM-DD', 'Somewhere, USA'),
+('Leigh Bardugo', '1975-04-06', 'Jerusalem, Israel'),
+('Julie Kagawa', '1982-10-12', 'Sacramento, CA, United States'),
+('Robert C. Martin', '1952-12-04', 'United States'),
+('Cathy ONeill', NULL, NULL),
+('Frank Herbert', '1920-10-08', 'Tacoma, WA, United States');
