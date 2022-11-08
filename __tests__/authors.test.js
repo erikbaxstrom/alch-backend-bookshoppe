36ba7cb9799f4810acce13c811c6e16b5228d150
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-07 17:16:52 -0800

    implement /authors test

---
+++ __tests__/authors.test.js
@@ -0,0 +1,22 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('Authors Routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('GET /authors should return the list of authors (id and name)', async () => {
+    const resp = await request(app).get('/authors');
+    expect(resp.status).toBe(200);
+    expect(resp.body.length).toBe(5);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(Number),
+      name: expect.any(String),
+    });
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
