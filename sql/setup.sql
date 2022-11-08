6c0fd8d9632b4337f7507482aada1766e7f94637
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-08 09:44:07 -0800

    update table name

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS author CASCADE;
+DROP TABLE IF EXISTS authors CASCADE;
 
-CREATE TABLE author (
+CREATE TABLE authors (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR NOT NULL,
     dob DATE,
@@ -10,7 +10,7 @@ CREATE TABLE author (
 );
 
 INSERT INTO 
-    author (name, dob, pob)
+    authors (name, dob, pob)
 VALUES
 -- ('Namy Name', 'YYYY-MM-DD', 'Somewhere, USA'),
 ('Leigh Bardugo', '1975-04-06', 'Jerusalem, Israel'),
