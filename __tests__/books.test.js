9346f13a08094f47b2f7230a3a191ef656d6b497
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-08 16:53:23 -0800

    implement /books route

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -9,7 +9,6 @@ describe('Books Routes', () => {
   });
   it('GET /books should return a list of books (id, title, release year)', async () => {
     const resp = await request(app).get('/books');
-
     expect(resp.status).toBe(200);
     expect(resp.body.length).toBe(14);
     expect(resp.body[0]).toEqual({
